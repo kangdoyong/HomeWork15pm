e372052a628f36b309b88aee660ca11556bd5af2
kangdoyong <166213018+kangdoyong@users.noreply.github.com>
2024-08-09 17:51:16 +0900

    08/09

---
+++ 에어컨클래스설계.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Numerics;
+
+public class 에어컨클래스설계
+{
+    static void Main()
+    {
+        AirConditioner airConditioner = new AirConditioner("Off", 20);
+        airConditioner.PowerOn();
+        airConditioner.PowerInfo();
+        airConditioner.TempUp();
+        airConditioner.TempInfo();
+        airConditioner.TempDown();
+        airConditioner.TempInfo();
+        airConditioner.PowerOff();
+        airConditioner.PowerInfo();
+    }
+}
+
+public class AirConditioner
+{
+    private string power;
+    private int temp = 20;
+
+    public AirConditioner(string power, int temp)
+    {
+        this.power = power;
+        this.temp = temp;
+    }
+
+    public void PowerOn()
+    {
+        Console.WriteLine("전원 켜기");
+        power = "On";
+    }
+
+    public void PowerOff()
+    {
+        Console.WriteLine("전원 끄기");
+        power = "Off";
+    }
+
+    public void TempUp()
+    {
+        Console.WriteLine("온도 올리기");
+        if (power == "On") ++temp;
+    }
+
+    public void TempDown()
+    {
+        Console.WriteLine("온도 내리기");
+        if (power == "On") --temp;
+
+    }
+
+    public void PowerInfo()
+    {
+        Console.WriteLine($"전원 상태 : {power}");
+    }
+
+    public void TempInfo()
+    {
+        Console.WriteLine($"설정된 온도 : {temp}");
+    }
+}
