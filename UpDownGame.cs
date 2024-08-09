451b0e34d5f237d9c984133fa91e5807a3954245
kangdoyong <166213018+kangdoyong@users.noreply.github.com>
2024-08-09 17:58:00 +0900

    08/09 수정 필요

---
+++ UpDownGame.cs
@@ -0,0 +1,30 @@
+﻿public class UpDownGame
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int chance = 5;
+        int randomNumber = random.Next(1, 100);
+
+        Console.Write("숫자 입력 : ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < randomNumber) Console.WriteLine("UP!");
+        else if (number == randomNumber)
+        {
+            Console.WriteLine($"정답!");
+            Console.WriteLine($"남은 기회 : {}");
+            Console.WriteLine($"정답 : {}");
+        }
+        else Console.WriteLine("DOWN!");
+
+
+        switch (number)
+        {
+            case ():
+                Console.WriteLine("UP!");
+                break;
+        }
+    }
+}
