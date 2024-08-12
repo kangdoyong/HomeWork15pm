7b4b091ec7857482de09f7317561d06b344486fa
kangdoyong <166213018+kangdoyong@users.noreply.github.com>
2024-08-12 15:04:26 +0900

    08/12

--- UpDownGame.cs
+++ UpDownGame.cs
@@ -22,14 +22,12 @@ public class UpDownGame
                 Console.WriteLine();
                 Console.WriteLine("정답!");
                 Console.WriteLine($"정답은 {randomNumber}, 남은기회는 {chance - i}");
-                return;
             }
 
             if (i == chance)
             {
                 Console.WriteLine();
                 Console.WriteLine($"실패! 정답은 {randomNumber}");
-                return;
             }
         }
 
